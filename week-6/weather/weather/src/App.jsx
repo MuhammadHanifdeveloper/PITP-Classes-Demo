fe256fe98e32a9e07053899fc9335627f0e91e89
Muhammad Hanif <muhammadhanif121212@outlook.com>
2025-12-31 16:44:44 +0500

    Update App.jsx

--- week-6/weather/weather/src/App.jsx
+++ week-6/weather/weather/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
     try {
       const response = await fetch(
-        `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
+        `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`
       );
 
       if (!response.ok) {
